883abee00442b5cbc6c350e102c0786c5f13a412
mohinimore545 <96488226+mohinimore545@users.noreply.github.com>
2025-10-25 19:22:53 +0530

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -18,10 +18,17 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New())
+	#app.Use(cors.New(cors.Config{
+	#	AllowOrigins: os.Getenv("ALLOWED_ORIGINS"),
+	#	AllowHeaders: "Origin, Content-Type, Accept",
+	#}))
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: os.Getenv("ALLOWED_ORIGINS"),
-		AllowHeaders: "Origin, Content-Type, Accept",
-	}))
+    AllowOrigins:     "http://65.1.124.134:3000, http://localhost:3000", 
+    AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+    AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+    AllowCredentials: true,
+}))
+
 	db := initializeDatabaseConnection()
 	repository.RunMigrations(db)
 	employeeRepository := repository.NewEmployeeRepository(db)
